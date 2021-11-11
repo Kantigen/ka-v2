d094124b4f106af13c2a8e680ffdd34b4f5e1f0a
Nathan McCallum <hey@nathan-mccallum.com>
2021-11-11 15:53:56 +1000

    Empire/login returns the empire status block

--- responses/empire/login.php
+++ responses/empire/login.php
@@ -2,11 +2,11 @@
 
 namespace KA\Empire;
 
-require_once __DIR__ . '/../get-status.php';
+require_once __DIR__ . '/get-status.php';
 
 function login() {
   return [
     'session_id' => 'this_is_a_session_id',
-    'status' => \KA\get_status(), // TODO: I think this should only return empire/get_status
+    'status' => \KA\Empire\get_status(),
   ];
 }
