bbf83179c6af4a8e443bfddf1c941cf43cf0c717
Nathan McCallum <hey@nathan-mccallum.com>
2021-11-29 23:47:02 +1000

    Fix incorrect star map size in server status response

--- ka/server/status-block.php
+++ ka/server/status-block.php
@@ -6,7 +6,11 @@ function status_block() {
   return [
     'time' => date(\KA\KenoAntigen::$DATE_FORMAT),
     'version' => '1.0',
-    'star_map_size' => 500,
+    'star_map_size' => [
+      'x' => [ -15, 15 ],
+      'y' => [ -15, 15 ],
+      'z' => [ -15, 15 ]
+    ],
     'rpc_limit' => 10000,
   ];
 }
